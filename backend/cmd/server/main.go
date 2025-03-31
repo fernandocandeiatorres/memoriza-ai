19e83244f638a94e5297d53e3290303a4c3dbbe6
Fernando Torres <fernandotorrescandeia@gmail.com>
2025-03-31 17:26:36 -0300

    go setup + main

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	err := config.LoadEnv()
+	if err != nil {
+		log.Printf("Warning: No .env file found or failed to load: %v", err)
+	}
+
+	router := gin.Default()
+
+	router.POST("/generate-flashcards", handler.GenerateFlashcardsHandler)
+
+	port := os.Getenv("POST")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Server Run Failed:", err)
+	}
+}
